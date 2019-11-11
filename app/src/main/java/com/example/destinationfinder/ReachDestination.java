a7e5abd7b3b76661a81103be66e02bb5fb35d01d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-11-11 11:43:36 +0500

    Added a Logo Screen With his Rating

---
+++ app/src/main/java/com/example/destinationfinder/ReachDestination.java
@@ -0,0 +1,14 @@
+package com.example.destinationfinder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReachDestination extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reach_destination);
+    }
+}
